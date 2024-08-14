959302129e17bed5b0884786c158b2aebb59ab85
mahmudmhb1 <mahmud.mhb1@gmail.com>
2024-08-14 22:29:56 +0600

    install requierd tools

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,7 @@
-/* eslint-disable no-undef */
-/* eslint-disable no-unused-vars */
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import cors from 'cors';
-import express, { Application, Request, Response } from 'express';
-import globalErrorHandler from './app/middlewares/globalErrorhandler';
-import notFound from './app/middlewares/notFound';
-import router from './app/routes';
+import cors from "cors";
+import express, { Application, Request, Response } from "express";
+
+import router from "./app/routes";
 
 const app: Application = express();
 
@@ -15,20 +10,19 @@ app.use(express.json());
 app.use(cors());
 
 // application routes
-app.use('/api', router);
+// app.use("/api", router);
 
 const test = async (req: Request, res: Response) => {
   const a = 10;
   res.send(a);
 };
 
-app.get('/', test);
-
+app.get("/", test);
 
 //Global Error Handler
-app.use(globalErrorHandler);
+// app.use(globalErrorHandler);
 
 //Not Found
-app.use(notFound);
+// app.use(notFound);
 
 export default app;
