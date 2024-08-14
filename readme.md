406e358327752101e27672a49b5d1358042451ef
sakib <sakkib0081@gmail.com>
2024-08-14 18:06:07 +0600

    first commit

---
+++ readme.md
@@ -0,0 +1,5 @@
+NODE_ENV=development
+PORT=5000
+DATABASE_URL=mongodb+srv://visa-thing:KAC8Sj6zCdB6pmx7@cluster0.b5kfivm.mongodb.net/visa-thing?retryWrites=true&w=majority&appName=Cluster0
+BCRYPT_SALT_ROUNDS=8
+
---
+++ src/app/routes/index.ts
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import { UserRoutes } from '../modules/user/user.route';
+
+const router = Router();
+
+const moduleRoutes = [
+  {
+    path: '/users',
+    route: UserRoutes,
+  },
+ 
+];
+
+moduleRoutes.forEach((route) => router.use(route.path, route.route));
+
+export default router;
